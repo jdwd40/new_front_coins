9d3f4d63d1b1b3ba85bdc38ac7004d6c4a191e45
jdwd40 <jd@jdwd40.com>
2023-05-16 17:48:00 +0100

    added simple user login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,86 @@
-import React, { Component } from 'react'
+import { useNavigate } from 'react-router-dom';
+import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  useToast,
+} from '@chakra-ui/react';
+import { useState, useContext } from 'react';
+import axios from 'axios';
+import { AuthContext } from '../contexts/AuthContext';
 
-export class Login extends Component {
-  render() {
-    return (
-      <div>Login</div>
-    )
-  }
+function Login() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { setUser } = useContext(AuthContext);
+  const toast = useToast();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        'http://localhost:9090/api/user/login',
+        { email, password }
+      );
+      // console.log(response);
+
+      // Store the JWT in the user's browser
+      localStorage.setItem('token', response.token);
+      // Set the user's data in the AuthContext
+      const user = await axios.get(`http://localhost:9090/api/user/getemail/${email}`) 
+
+      setUser(user.data);
+      // Show a success toast
+      toast({
+        title: 'Logged in successfully.',
+        status: 'success',
+        duration: 3000,
+        isClosable: true,
+      });
+
+      // Redirect the user to the home page (or wherever you want)
+      navigate('/');
+      // You'll need to use the useHistory hook from react-router-dom
+    } catch (error) {
+      // Show an error toast
+      console.log(error);
+      toast({
+        title: 'Failed to log in.',
+        // description: error.response.data.message,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      });
+    }
+  };
+
+  return (
+    <Box as="form" onSubmit={handleSubmit}>
+      <FormControl id="email">
+        <FormLabel>Email</FormLabel>
+        <Input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </FormControl>
+      <FormControl id="password">
+        <FormLabel>Password</FormLabel>
+        <Input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </FormControl>
+      <Button mt={4} colorScheme="teal" type="submit">
+        Login
+      </Button>
+    </Box>
+  );
 }
 
-export default Login
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,41 @@
-import React, { Component } from 'react'
+import {
+  Box,
+  Button,
+  Center,
+  FormControl,
+  FormLabel,
+  Input,
+  Heading,
+} from '@chakra-ui/react';
 
-export class Register extends Component {
-  render() {
-    return (
-      <div>Register</div>
-    )
-  }
+function Register() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // handle form submission...
+  };
+
+  return (
+    <Center>
+      <Box as="form" onSubmit={handleSubmit}>
+        <Heading mb="2">Register Your Account</Heading>
+        <FormControl id="email">
+          <FormLabel>Email</FormLabel>
+          <Input type="email" />
+        </FormControl>
+        <FormControl id="password">
+          <FormLabel>Password</FormLabel>
+          <Input type="password" />
+        </FormControl>
+        <FormControl id="confirmPassword">
+          <FormLabel>Confirm Password</FormLabel>
+          <Input type="password" />
+        </FormControl>
+        <Button mt={4} colorScheme="teal" type="submit">
+          Register
+        </Button>
+      </Box>
+    </Center>
+  );
 }
 
-export default Register
+export default Register;
